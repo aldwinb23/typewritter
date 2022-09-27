6a90493cb03364877e5bd6f434e9c317706567e8
aldwinb <aldwinb23@gmail.com>
2022-09-27 15:39:40 -0700

    intial commit of typewritter.js

---
+++ typewriter.js
@@ -0,0 +1,14 @@
+
+const sentence = "hello there from lighthouse labs\n";
+
+let delay = 500
+
+for (const char of sentence) {
+
+  setTimeout(() => {
+    process.stdout.write(char);
+  }, delay);
+
+  delay += 100
+
+}
